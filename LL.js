e11aa82ba827a79ddb0a2b690ad19b8eb96594cd
Rene Montoya <rmontoya.we@gmail.com>
2021-01-24 17:32:00 -0800

    finishing three algos

--- LL.js
+++ LL.js
@@ -28,6 +28,34 @@ class SLL {
         this.head = this.head.next;
         return this.head;
     }
+    contains(value) {
+        var runner = this.head;
+        while (runner != null) {
+            if (runner.value == value) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+    length() {
+        var runner = this.head;
+        var counter = 0;
+        while (runner != null) {
+            counter++;
+            runner = runner.next;
+        }
+        return counter;
+    }
+    display() {
+        var runner = this.head;
+        var str = "";
+        while (runner != null) {
+            str += runner.value + " ";
+            runner = runner.next;
+        }
+        return str;
+    }
 }
 
 var myNode1 = new Node("Reptar");
@@ -41,3 +69,6 @@ myNode2.next = myNode3;
 console.log(sll1.front());
 // console.log(sll1.removeFront());
 console.log(sll1.addFront("Dill"));
+// console.log(sll1.contains("John"));
+// console.log(sll1.length());
+console.log(sll1.display());
